8ee5288bf8857878aaa908e1fb56aceb9bebacd4
alexandreleao <erdleosalgueiro@gmail.com>
2022-05-03 17:15:04 -0300

    Terceiro commit

--- app/Controllers/Http/HomeController.ts
+++ app/Controllers/Http/HomeController.ts
@@ -1,9 +1,10 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class HomeController {
 
- async index({view}){
-    return view.render('painel/homepage');
+ async index( ctx: HttpContextContract){
+    
+    return ctx.view.render('painel/homepage');
  }
 
  async sobre(){
